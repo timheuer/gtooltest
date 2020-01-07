220c70dfb5f0cfe5ced9bbd338a8eed6741ff6a2
Tim Heuer <tim@timheuer.com>
2020-01-07 12:51:33 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace gtooltest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if NETCOREAPP2_1
+            Console.WriteLine("Hello World from 2.1!");
+#elif NETCOREAPP3_1
+            Console.WriteLine("Hello world from 3.1!");
+#else
+            Console.WriteLine("Hello world");
+#endif
+        }
+    }
+}
